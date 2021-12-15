b21235ea3e052c840ad0b2f190f13d665e518608
Aleksandar Velichkovikj <al.velichkovikj@gmail.com>
2021-12-15 15:37:52 +0100

    c

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.use('/auth', auth)
 const concerts = require('./routes/concerts')
 app.use('/api', concerts)
 
+const path = require('path')
+app.use(express.static(path.join(__dirname, '/client/build')))
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + '/client/build/index.html')
+})
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
 
